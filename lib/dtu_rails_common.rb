509dbe8aff2f21feb3fb6bc93e10469ac643bd7d
Jimmy Petersen <jipe@dtic.dtu.dk>
2014-12-02 11:57:00 +0100

    Only require runtime dependencies from gemspec

--- lib/dtu_rails_common.rb
+++ lib/dtu_rails_common.rb
@@ -1,6 +1,6 @@
 require "dtu_rails_common/engine"
 
-Gem.loaded_specs['dtu_rails_common'].dependencies.each do |d|
+Gem.loaded_specs['dtu_rails_common'].runtime_dependencies.each do |d|
   require d.name
 end
 
